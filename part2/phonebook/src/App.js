79ae912edbaed1e22dcf61a972a2a71e4362875f
nicknickovich <nick.poberezhnyk@gmail.com>
2020-03-27 19:51:18 +0200

    Completed exercises 2.6, 2.7

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    {
+      name: 'Arto Hellas',
+      id: 1
+    }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName,
+      id: persons.length + 1
+    }
+    const allPersonsNames = persons.map(
+      person => person.name
+    )
+    if (allPersonsNames.includes(newPerson.name)) {
+      alert(`${newPerson.name} is already in the phonebook`)
+    } else {
+      setPersons(persons.concat(newPerson))
+    }
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        <ul>
+          {persons.map(person =>
+            <Person name={person.name} key={person.id}/>
+          )}
+        </ul>
+    </div>
+  )
+}
+
+export default App
