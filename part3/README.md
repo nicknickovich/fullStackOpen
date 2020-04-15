aebbc2a88abd0b915cde548607188b7c105bf59f
nicknickovich <nick.poberezhnyk@gmail.com>
2020-04-15 11:48:46 +0300

    Complete exercise 3.10

---
+++ part3/README.md
@@ -0,0 +1,3 @@
+app url:
+http://guarded-spire-39952.herokuapp.com
+[link](http://guarded-spire-39952.herokuapp.com)
--- part3/index.js
+++ part3/index.js
@@ -88,7 +88,7 @@ app.get('/info', (request, response) => {
   )
 })
 
-const port = 3002
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
