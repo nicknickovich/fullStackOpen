d74d34fbd3e52957bd26c383b04fdf476dca5004
nicknickovich <nick.poberezhnyk@gmail.com>
2020-04-16 12:08:57 +0300

    Complete exercises 3.11-3.12

---
+++ part2/phonebook/build/precache-manifest.71b70416f004bd1b7bddbdff01b22dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776fc00fe5391e96a600f1e0f74e40ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54b4b5f92bd877f50e16",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "a2c56da8308a66b9529d",
+    "url": "/static/js/2.d50c3fcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d50c3fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b4b5f92bd877f50e16",
+    "url": "/static/js/main.c56803cf.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
