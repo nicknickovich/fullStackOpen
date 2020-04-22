3caaf09e09e9991cffa7b5445904c6cd3cb4f82a
nicknickovich <nick.poberezhnyk@gmail.com>
2020-04-22 11:45:44 +0300

    Complete exercise 4.4

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -6,3 +6,35 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  test('empty array should return zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+  test('array of one element has as many likes', () => {
+    const blogs = [
+      {
+        id: 1,
+        likes: 1
+      }
+    ]
+    expect(listHelper.totalLikes(blogs)).toBe(1)
+  })
+  test('array of three elements', () => {
+    const blogs = [
+      {
+        id: 1,
+        likes: 1
+      },
+      {
+        id: 2,
+        likes: 2
+      },
+      {
+        id: 3,
+        likes: 3
+      }
+    ]
+    expect(listHelper.totalLikes(blogs)).toBe(6)
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  const reducer = (accumulator, currentValue) => accumulator + currentValue['likes']
+  return blogs.reduce(reducer, 0)
+}
+
+module.exports = { dummy, totalLikes }
