a1747d5695665997cead80df0a7ef9bdb2b259d5
nicknickovich <nick.poberezhnyk@gmail.com>
2020-06-03 10:48:21 +0300

    Complete exercise 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -32,10 +32,13 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: `${error.message}` } )
+    return response.status(400).json({ error: error.message } )
   }
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  }
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
   }
   next(error)
 }
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,16 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate(
@@ -23,8 +32,15 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
 
-  const user = await User.findById(body.author)
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     _id: body._id,
